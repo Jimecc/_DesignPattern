b6188a17022cdbf35ec97ab13a3da7cf343d4578
Jimecc <1813085460@qq.com>
2022-08-13 00:02:45 +0800

    OpenClosedPrinciple

---
+++ src/com.aiguigu.principle/Demeter/Demeter.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jim
+ * @Description 迪米特法则
+ * @createTime 2022年08月12日
+ */
+public class Demeter {
+    public static void main(String[] args) {
+        SchoolManager schoolManager = new SchoolManager();
+        schoolManager.printAllEmployee(new CollegeManager());
+    }
+}
+
+class Employee{
+    private String id;
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getId(){
+        return id;
+    }
+}
+
+
+class CollegeEmployee{
+    private String id;
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getId(){
+        return id;
+    }
+}
+
+class CollegeManager{
+    public List<CollegeEmployee> getAllEmployee(){
+        List<CollegeEmployee> list = new ArrayList<>();
+        for(int i =0;i<10;i++){
+            CollegeEmployee emp = new CollegeEmployee();
+            emp.setId("学院员工id="+i);
+            list.add(emp);
+        }
+        return list;
+    }
+}
+
+class SchoolManager{
+    public List<Employee> getAllEmployee(){
+        List<Employee> list = new ArrayList<>();
+
+        for(int i = 0;i<6;i++) {
+            Employee employee = new Employee();
+            employee.setId("学校总部员工id="+i);
+            list.add(employee);
+        }
+
+        return list;
+
+
+    }
+
+    void printAllEmployee(CollegeManager sub){
+        List<CollegeEmployee> list1 = sub.getAllEmployee();
+        System.out.println("----------分公司员工----------");
+        for (CollegeEmployee collegeEmployee : list1) {
+            System.out.println(collegeEmployee.getId());
+        }
+
+        List<Employee> list2 = this.getAllEmployee();
+        System.out.println("----------学校总部员工----------");
+        for (Employee employee : list2) {
+            System.out.println(employee.getId());
+
+        }
+    }
+
+
+
+}
